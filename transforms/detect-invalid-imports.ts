4f43dd8c974c3426b31c739ed5300a7c5f8f938b
Evilweed <mcierpicki@gmail.com>
2021-04-23 00:13:24 +0200

    feat[ext] step 2

--- transforms/detect-invalid-imports.ts
+++ transforms/detect-invalid-imports.ts
@@ -1,25 +1,12 @@
 import {API, FileInfo, Options} from 'jscodeshift';
 import {ASTPath, JSCodeshift, CatchClause} from "jscodeshift/src/core";
 
-function addCommentToNode(codeApi: JSCodeshift, node, text: string) {
-  const comment = codeApi.commentLine(text, true, false);
-  const comments = node.node.comments = node.node.comments || [];
-  comments.push(comment);
-}
-
 export default function someNewTransform(file: FileInfo, api: API, options: Options) {
   const jscodeshift: JSCodeshift = api.jscodeshift
   const root = jscodeshift(file.source)
 
-  const updatedAnything = root                           // In root
-    .find(jscodeshift.CatchClause)                       // find all "catch {}"
-    .forEach((catchClause: ASTPath<CatchClause>) => {    // for each found
-      return addCommentToNode(                           // add comment above
-        jscodeshift,
-        catchClause,
-        'Found catch! :)'
-      )
-    })
+  const updatedAnything = root
+    .find(jscodeshift.CatchClause)
 
   return updatedAnything ? root.toSource() : null;
 }
