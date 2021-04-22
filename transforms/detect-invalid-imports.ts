2ae0922f9a282cd2746c552d1d5b92d5f12c43e1
Evilweed <mcierpicki@gmail.com>
2021-04-23 00:15:17 +0200

    feat[ext] step 3

--- transforms/detect-invalid-imports.ts
+++ transforms/detect-invalid-imports.ts
@@ -1,12 +1,12 @@
 import {API, FileInfo, Options} from 'jscodeshift';
-import {ASTPath, JSCodeshift, CatchClause} from "jscodeshift/src/core";
+import {ASTPath, JSCodeshift, ImportDeclaration} from "jscodeshift/src/core";
 
 export default function someNewTransform(file: FileInfo, api: API, options: Options) {
   const jscodeshift: JSCodeshift = api.jscodeshift
   const root = jscodeshift(file.source)
 
   const updatedAnything = root
-    .find(jscodeshift.CatchClause)
+    .find<ImportDeclaration>(jscodeshift.ImportDeclaration)
 
   return updatedAnything ? root.toSource() : null;
 }
