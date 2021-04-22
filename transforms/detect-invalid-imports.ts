650a0a8e5173341b797d46f1fe86e03f5a95b58a
Evilweed <mcierpicki@gmail.com>
2021-04-23 00:18:14 +0200

    feat[ext] step 5

--- transforms/detect-invalid-imports.ts
+++ transforms/detect-invalid-imports.ts
@@ -1,6 +1,10 @@
 import {API, FileInfo, Options} from 'jscodeshift';
 import {ASTPath, JSCodeshift, ImportDeclaration, StringLiteral} from "jscodeshift/src/core";
 
+function isImportingFromCreator(path) {
+  return path.indexOf('/creator/') > -1;
+}
+
 export default function someNewTransform(file: FileInfo, api: API, options: Options) {
   const jscodeshift: JSCodeshift = api.jscodeshift
   const root = jscodeshift(file.source)
@@ -8,6 +12,7 @@ export default function someNewTransform(file: FileInfo, api: API, options: Opti
   const updatedAnything = root
     .find<ImportDeclaration>(jscodeshift.ImportDeclaration)
     .find<StringLiteral>(jscodeshift.StringLiteral)
+    .filter((node) => isImportingFromCreator(node.value.value))
 
   return updatedAnything ? root.toSource() : null;
 }
