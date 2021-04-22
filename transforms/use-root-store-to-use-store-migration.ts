a13bf904f1c320d270740042b2a2c21cd1d63b72
Evilweed <mcierpicki@gmail.com>
2021-04-23 00:46:22 +0200

    feat[ext] step 4

--- transforms/use-root-store-to-use-store-migration.ts
+++ transforms/use-root-store-to-use-store-migration.ts
@@ -1,25 +1,14 @@
 import {API, FileInfo, Options} from 'jscodeshift';
-import {ASTPath, JSCodeshift, CatchClause} from "jscodeshift/src/core";
-
-function addCommentToNode(codeApi: JSCodeshift, node, text: string) {
-  const comment = codeApi.commentLine(text, true, false);
-  const comments = node.node.comments = node.node.comments || [];
-  comments.push(comment);
-}
+import {ASTPath, JSCodeshift} from "jscodeshift/src/core";
 
 export default function someNewTransform(file: FileInfo, api: API, options: Options) {
   const jscodeshift: JSCodeshift = api.jscodeshift
   const root = jscodeshift(file.source)
 
-  const updatedAnything = root                           // In root
-    .find(jscodeshift.CatchClause)                       // find all "catch {}"
-    .forEach((catchClause: ASTPath<CatchClause>) => {    // for each found
-      return addCommentToNode(                           // add comment above
-        jscodeshift,
-        catchClause,
-        'Found catch! :)'
-      )
-    })
+  const updatedAnything = root
+    .find(jscodeshift.CatchClause)
+  //                     \___ sooo, how should i know how things are named? :<
+  //                          https://astexplorer.net to the rescue!
 
   return updatedAnything ? root.toSource() : null;
 }
